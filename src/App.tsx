eb6ec0a375f68cbf4113129dc519b93eeb4d7bcf
roee ltmanovich <roeeli770@gmail.com>
2024-11-21 17:02:39 +0200

    begin to work on register

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import './App.css'
-import Layout from './components/Layout/Layout'
-import NavBar from './components/NavBar/NavBar'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import NavBar from "./components/NavBar/NavBar";
+import Register from "./pages/Register/Register";
 
 function App() {
-
-
-  return (
-    <div className='app'>
-      <Layout/>
-    </div>
-  )
+    return (
+        <div className="app">
+            <Layout />
+            <Routes>
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Login from '../Login/Login'
+import "./Register.css"
+import { useAppDispatch, useAppSelector } from '../../redux/store';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+
+  const { user } = useAppSelector((state) => state.user);
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>Register</div>
+    <div className="register">
+      <input placeholder='name' type="text" />
+      <input placeholder='password' type="password" />
+      <input type="text" />
+      <button>register</button>
+    </div>
   )
 }
