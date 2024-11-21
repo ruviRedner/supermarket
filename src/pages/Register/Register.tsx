2abb1ca4590de1fc8545e5def60455b2b8ffbfbc
roee ltmanovich <roeeli770@gmail.com>
2024-11-21 17:16:22 +0200

    add creditCard in fech register

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -3,21 +3,30 @@ import Login from '../Login/Login'
 import "./Register.css"
 import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { useNavigate } from 'react-router-dom';
+import { fetchRegister } from '../../redux/slices/userSlice';
 
 export default function Register() {
 
-  const { user } = useAppSelector((state) => state.user);
+  const { status  } = useAppSelector((state) => state.user);
   const navigate = useNavigate()
   const dispatch = useAppDispatch();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [password, setPassword] = useState("");
+  const [creditCard, setCreditCard] = useState("");
+
+  const hendelInRegister = () => {
+    dispatch(fetchRegister({
+      username, password,
+    })
+  )
+    navigate("/login");
+};
 
   return (
     <div className="register">
-      <input placeholder='name' type="text" />
-      <input placeholder='password' type="password" />
-      <input type="text" />
+      <input value={username} placeholder='name' type="text" />
+      <input value={password} placeholder='password' type="password" />
+      <input value={creditCard} type="text" />
       <button>register</button>
     </div>
   )
