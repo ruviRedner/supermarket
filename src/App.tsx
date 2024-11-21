e547ff1c17ccab25abebb6b927c8371a2c5de3e7
meirgoldshtein <m0548491057@gmail.com>
2024-11-21 14:27:50 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout/Layout'
 import NavBar from './components/NavBar/NavBar'
+import Home from './pages/Home/Home'
 
 function App() {
 
 
   return (
     <div className='app'>
-      <Layout/>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </Layout>
+
     </div>
   )
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import NavBar from '../NavBar/NavBar'
 import Logo from '../Logo/Logo'
 import './Layout.css'
-
-export default function Layout() {
+interface Props {
+  children: React.ReactNode
+}
+export default function Layout({ children }: Props) {
   return (
     <div className='layout-container'>
       <div className="header">
         <NavBar />
         <Logo />
+        {children}
       </div>
 
     </div>
