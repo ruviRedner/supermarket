05d38fb1eea79ee34597cea631ccb4364133c91a
ruviRedner <r.y.r7966@gmail.com>
2024-11-21 17:21:59 +0200

    component login done

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../redux/store";
+import {  useState } from "react";
+import { fetchLogin } from "../../redux/slices/userSlice";
+import "./login.css"
 
-export default function Login() {
+const Login = () => {
+  const dispatch = useAppDispatch();
+  
+  
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const handelLogin = () =>{
+    dispatch(fetchLogin({username,password}))
+  }
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <input id="inp"
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setusername(e.target.value)}
+      />
+      <input id="inp"
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setpassword(e.target.value)}
+      />
+      <button onClick={handelLogin}>Login</button>
+
+      <p>
+        Don't have an accunt yet?
+        <Link to={"/register"}>Register</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
