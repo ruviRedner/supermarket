a6296f25421ba94dfe63abefbb70169271dc3582
meirgoldshtein <m0548491057@gmail.com>
2024-11-21 15:36:22 +0200

    added styling to categoryes

--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import './CategoryCard.css'
 interface Props {
   name: string
   image: string
 }
 export default function CategoryCard({ name, image }: Props) {
   return (
-    <div className='category-card-container'>
-      <img src={image} alt={name} />
+    <div className='category-card'>
+      <img src={image} alt={name} className='image'/>
       <p>{name}</p>
     </div>
   )
