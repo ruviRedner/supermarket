443f9c1ebf912728ac668441e057a559a4c6a47e
ruviRedner <r.y.r7966@gmail.com>
2024-11-24 10:24:51 +0200

    product list getting done

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { RootState, useAppDispatch, useAppSelector } from '../../redux/store';
+import { fetchAllProducts } from '../../redux/slices/productSlice';
+import { IProduct } from '../../types/product';
+import ProductCard from '../ProductCard/ProductCard';
 
 export default function ProductList() {
+  const productList = useAppSelector((state: RootState) => state.products.data);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, []);
+
   return (
-    <div>ProductList</div>
-  )
+    <div>
+      {productList?.length > 0 ? (
+        productList.map((pro: IProduct) => <ProductCard key={pro._id} product={pro}/>)
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
