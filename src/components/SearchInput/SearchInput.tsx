e55fc0877fd2da61dd413e593d07ffc75cc3ef62
roee ltmanovich <roeeli770@gmail.com>
2024-11-24 12:25:04 +0200

    add base component for search by name

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearchInput.css'
+import { useAppDispatch, useAppSelector } from '../../redux/store'
+import { fetchByName } from '../../redux/slices/productSlice'
 export default function SearchInput() {
+  const dispatch = useAppDispatch()
+  const {data} = useAppSelector((state) => state.products)
+  const [name, setName] = useState<string>("")
+
+  const hendelInSearchByName = async () => {  
+    await dispatch(fetchByName(name))
+    console.log(data);
+    
+    if(!data) return
+  }
   return (
     <div className='search-container'>
-      <input type="text" placeholder='Search' />
-      <div className="search-icon">🔍</div>
+      <input value={name} onChange={(e)=> {setName(e.target.value)}} type="text" placeholder='Search' />
+      <div onClick={hendelInSearchByName}  className="search-icon">🔍</div>
     </div>
   )
 }
