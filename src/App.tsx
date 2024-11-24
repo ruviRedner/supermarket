c8abf079a008221be55c90d5034a9023b4bc7157
ruviRedner <r.y.r7966@gmail.com>
2024-11-24 14:36:59 +0200

    prettier and socket connction

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,34 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Layout from './components/Layout/Layout'
-import NavBar from './components/NavBar/NavBar'
-import Home from './pages/Home/Home'
-import Register from './pages/Register/Register'
-import Login from './pages/Login/Login'
-import MyCart from './pages/MyCart/MyCart'
-import ProductList from './components/ProductList/ProductList'
-import Contact from './pages/contact/Contact'
-import Pay from './pages/Pay/Pay'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import MyCart from "./pages/MyCart/MyCart";
+import ProductList from "./components/ProductList/ProductList";
+import Contact from "./pages/contact/Contact";
+import Pay from "./pages/Pay/Pay";
+import io from "socket.io-client";
+export const socket = io("http://localhost:3000");
 function App() {
-
-
   return (
-    <div className='app'>
+    <div className="app">
       <Layout>
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='*' element={<div className='not-found'>404</div>} />
-          <Route path='register' element={<Register/>} />
-          <Route path='login' element={<Login/>} />
-          <Route path='logout' element={<div>logout</div>} />
-          <Route path='my-cart' element={<MyCart/>} />
-          <Route path='contact' element={<Contact/>} />
-      
-          <Route path='checkout' element={<Pay/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<div className="not-found">404</div>} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="logout" element={<div>logout</div>} />
+          <Route path="my-cart" element={<MyCart />} />
+          <Route path="contact" element={<Contact />} />
+
+          <Route path="checkout" element={<Pay />} />
         </Routes>
       </Layout>
-
     </div>
-  )
+  );
 }
 
 export default App;
