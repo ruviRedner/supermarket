d792b99a8068fc42e056863a4154791aa64d886f
Netanel Rotenstein <email@example.com>
2024-11-24 11:17:49 +0200

    route of contact

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>Send a comments and donates 💸 😜 to our community via https://kodcode2workspace.slack.com/archives/C0822CKJJDP</div>
+  )
+}
