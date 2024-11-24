185e18db52059b2a435322cde11299424ce4caf5
ruviRedner <r.y.r7966@gmail.com>
2024-11-24 10:25:05 +0200

    activate in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SearchInput from '../../components/SearchInput/SearchInput'
 import CategoryList from '../../components/CategoryList/CategoryList'
+import ProductList from '../../components/ProductList/ProductList'
 
 export default function Home() {
   return (
     <div className='home-container'>
       <SearchInput />
       <CategoryList />
+      <ProductList/>
     </div>
   )
 }
