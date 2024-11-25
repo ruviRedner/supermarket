7acc356b544b4a1c65f16ad497ec388004f01148
ruviRedner <r.y.r7966@gmail.com>
2024-11-25 14:15:28 +0200

    add socket

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,16 +3,27 @@ import SearchInput from '../../components/SearchInput/SearchInput'
 import CategoryList from '../../components/CategoryList/CategoryList'
 import ProductList from '../../components/ProductList/ProductList'
 import { RootState, useAppDispatch, useAppSelector } from '../../redux/store';
-import { fetchCart } from '../../redux/slices/cartSlice';
+import cartSlice from '../../redux/slices/cartSlice';
+import { socket } from '../../App';
+import { ICart } from '../../types/cart';
 
 export default function Home() {
   const dispatch = useAppDispatch();
   const user_id = useAppSelector((state: RootState) => state.user.user?._id);
   useEffect(() => {
+    const handleCartUpdated = (updatedCart: ICart) => {
+      console.log('Cart Data:', updatedCart)
+      dispatch(cartSlice.actions.setCart(updatedCart));
+    };
     if (user_id) {
-      dispatch(fetchCart(user_id));
+      
+      socket.emit("getCart",user_id)
     }
-  }, [dispatch, user_id]);
+    socket.on("cartUpdated", handleCartUpdated);
+    return () => {
+      socket.off("cartUpdated", handleCartUpdated)
+    }
+  }, [ user_id]);
   return (
     <div className='home-container'>
       <SearchInput />
