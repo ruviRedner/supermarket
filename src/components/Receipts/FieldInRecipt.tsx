46c38802bd1b0872bcdc1556b15c490169404333
ruviRedner <r.y.r7966@gmail.com>
2024-11-25 15:09:29 +0200

    css

--- src/components/Receipts/FieldInRecipt.tsx
+++ src/components/Receipts/FieldInRecipt.tsx
@@ -10,8 +10,13 @@ export default function FieldInRecipt({historyItem}:props) {
   
   return (
     <div className='receipt-items'>
+      <div className='receipt-date'>
+
         <h2>date: {historyItem.date.toString()}</h2>
+        </div>
+        <div className='receipt-total'>
         <h2>TotalPrice : {historyItem.totalPrice}$</h2>
+        </div>
     </div>
   )
 }
